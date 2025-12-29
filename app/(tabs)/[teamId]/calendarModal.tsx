0e2cde59c3a367a6bdcdd6ba9396b9a4790f4697
JiHyeon Lee <justjh30@gmail.com>
2025-12-29 13:53:48 +0900

    chore: 디자인 토큰 가공 CI/CD, 개발자 계정 연결 설정 (#14)
    * chore: 스토리북, 디자인 토큰 관련 패키지 설치 및 기본 폴더 구조 생성
    
    * chore: 푸시 패키지 설정, 구글 서비스 설정
    
    * chore: 애플 개발자 계정 연결
    
    * chore: 토큰 json 생성
    
    * chore: 디자인 토큰 가공 ci/cd 설정
    
    ---------
    
    Co-authored-by: kimywann <kimywan10@gmail.com>

---
+++ app/(tabs)/[teamId]/calendarModal.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+interface CalendarModalProps {}
+
+const CalendarModal = ({}: CalendarModalProps) => {
+  return <View></View>;
+};
+
+export default CalendarModal;
---
+++ shared/ui/molecules/Button.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+interface ButtonProps {}
+
+const Button = ({}: ButtonProps) => {
+  return <View></View>;
+};
+
+export default Button;
