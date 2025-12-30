e75e0ae195cb549ab8615a497e5007bfc6b1cb32
JiHyeon Lee <justjh30@gmail.com>
2025-12-30 13:09:56 +0900

    chore: 스토리북 CI/CD 설정 #6 (#19)
    * chore: 스토리북 설정 변경
    * chore:  스토리북 ci/cd 설정
    * style: 기본 스토리 생성

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,20 @@
-import type { StorybookConfig } from '@storybook/react-native-web-vite';
+import type { StorybookConfig } from "@storybook/react-native-web-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    "../shared/stories/**/*.mdx",
+    "../shared/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
   ],
-  "framework": "@storybook/react-native-web-vite"
+  framework: "@storybook/react-native-web-vite",
+  viteFinal: async (config) => {
+    config.base = "./";
+    return config;
+  },
 };
 export default config;
