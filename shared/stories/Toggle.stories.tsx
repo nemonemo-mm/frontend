f53e907a4facbad83165c896326293d4e3a1328b
SleepingOff <justjh30@gmail.com>
2025-12-30 17:10:07 +0900

    feat: 토글 컴포넌트 생성

---
+++ shared/stories/Toggle.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Toggle from "../ui/atoms/Toggle";
+
+const meta: Meta<typeof Toggle> = {
+  title: "atoms/Toggle",
+  component: Toggle,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Toggle>;
+
+export const Primary: Story = {};
---
+++ shared/ui/atoms/Toggle.tsx
@@ -0,0 +1,65 @@
+import { useRef, useState } from "react";
+import {
+  Animated,
+  GestureResponderEvent,
+  Pressable,
+  StyleSheet,
+} from "react-native";
+import {
+  globalBorderSubtle,
+  globalPrimaryMain,
+  globalSurfaceDefault,
+} from "..";
+
+interface ToggleProps {
+  value: boolean;
+}
+
+const Toggle = ({ value }: ToggleProps) => {
+  const [toggle, setToggle] = useState(value);
+
+  const animation = useRef(new Animated.Value(0)).current;
+  const translateX = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 16],
+  });
+  const handlePressToggle = (e: GestureResponderEvent) => {
+    e.preventDefault();
+    setToggle((prev) => !prev);
+    const nextValue = !toggle;
+    Animated.timing(animation, {
+      toValue: nextValue ? 1 : 0,
+      duration: 150,
+      useNativeDriver: true,
+    }).start();
+  };
+  return (
+    <Pressable
+      style={[style.toggle, toggle && style.pressedToggle]}
+      onPress={handlePressToggle}
+    >
+      <Animated.View style={[style.circle, { transform: [{ translateX }] }]} />
+    </Pressable>
+  );
+};
+
+const style = StyleSheet.create({
+  toggle: {
+    width: 36,
+    height: 20,
+    backgroundColor: globalBorderSubtle,
+    borderRadius: 9999,
+    padding: 2,
+  },
+  pressedToggle: {
+    backgroundColor: globalPrimaryMain,
+  },
+  circle: {
+    width: 16,
+    height: 16,
+    backgroundColor: globalSurfaceDefault,
+    borderRadius: 9999,
+  },
+});
+
+export default Toggle;
