42126ff5b8ba63bd977077b2dbe7c7d7106a3840
SleepingOff <justjh30@gmail.com>
2025-12-31 13:34:41 +0900

    feat: 칩 컴포넌트 생성

---
+++ shared/ui/atoms/Chip.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from "react";
+import { StyleSheet, Text } from "react-native";
+import {
+  globalPrimaryChip,
+  globalPrimarySoft,
+  globalSurfaceDisabledstroke,
+  globalSurfaceSubtle,
+  globalTextDisabled,
+} from "..";
+
+interface ChipProps {
+  children: ReactNode;
+  active: boolean;
+}
+
+const Chip = ({ children, active }: ChipProps) => {
+  return (
+    <Text style={[active ? style.active : style.inactive, style.chip]}>
+      {children}
+    </Text>
+  );
+};
+
+const style = StyleSheet.create({
+  chip: {
+    borderRadius: 9999,
+    borderWidth: 1,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    justifyContent: "center",
+    alignSelf: "flex-start",
+  },
+  inactive: {
+    borderColor: globalSurfaceDisabledstroke,
+    backgroundColor: globalSurfaceSubtle,
+    color: globalTextDisabled,
+  },
+  active: {
+    borderColor: globalPrimarySoft,
+    backgroundColor: globalPrimarySoft,
+    color: globalPrimaryChip,
+  },
+});
+export default Chip;
