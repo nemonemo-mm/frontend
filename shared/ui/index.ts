2d247cae361112df06b5e0a003e7874dfa06d3b5
kimywann <kimywan10@gmail.com>
2026-01-02 01:33:30 +0000

    design: 토큰 가공 결과

---
+++ shared/ui/index.ts
@@ -0,0 +1,81 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const globalColorCalendarLabelType1 = "#fe9689"; // 이번달 타입1
+export const globalColorCalendarLabelType1muted = "#f6b8ae"; // 지난달 타입1
+export const globalColorCalendarLabelType2 = "#ccaacb"; //  이번달 타입2
+// 지난달 타입2
+//
+export const globalColorCalendarLabelType2muted = "#e2d4e1";
+// 이번달 타입3
+//
+export const globalColorCalendarLabelType3 = "#b7cfb7";
+export const globalColorCalendarLabelType3muted = "#d8e6d8"; // 지난달 타입3
+export const globalColorCalendarLabelType4 = "#ffc7a2"; // 이번달 타입4
+export const globalColorCalendarLabelType4muted = "#ffe2cf"; // 지난달 타입4
+export const globalColorCalendarLabelType5 = "#acdee7"; // 이번달 타입5
+export const globalColorCalendarLabelType5muted = "#d6eef3"; // 지난달 타입5
+export const globalColorCalendarLabelType6 = "#f3b0c2"; // 이번달 타입6
+export const globalColorCalendarLabelType6muted = "#f8d3dd"; // 지난달 타입6
+export const globalDropdown = "0 -2px 16px 0 #00000040";
+export const globalTypographyH2 = "500 16px/18 pretendard";
+export const globalTypographyH1 = "500 18px/20 pretendard";
+export const globalTypographyH3 = "600 16px/20 pretendard";
+export const globalTypographyBody1 = "500 14px/16 pretendard";
+export const globalTypographyBody2 = "400 14px/16 pretendard";
+export const globalTypographyBody3 = "400 12px/16 pretendard";
+export const globalTypographyCaption = "600 12px/14 pretendard";
+export const globalTypographyTabinactive = "400 16px/18 pretendard";
+export const globalTypographyBody2Tight = "400 14px/16 pretendard";
+export const globalTypographyBody2wide = "400 14px/20 pretendard";
+export const globalSpacingXs = 8;
+export const globalSpacingSm = 12;
+export const globalSpacingMd = 16;
+export const globalSpacingLg = 20;
+export const globalSpacingXl = 24;
+export const globalSpacing2xl = 30;
+export const globalSpacingProfile = "36";
+export const globalSpacingTitletoProfile = "36";
+export const globalSpacingTitle = "64";
+export const globalSpacingIcontoTypo = "4";
+export const globalBmRadius = 20;
+export const globalGray900 = "#2b2b2b"; // 메인텍스트
+export const globalGray700 = "#5f5f5f"; // 보조텍스트, 선택 전 입력 상태 텍스트
+export const globalGray600 = "#6f6f6f"; // 안내/ 가이드/ 플레이스홀더
+// 읽은 알림
+// 캘린더 저번달 / 다음달 날짜
+// 과거 일정
+export const globalGray500 = "#8a8a8a";
+// 비활성 탭
+// CTA 비활성
+// 조작 불가 상태 라벨
+export const globalGray400 = "#9b9b9b";
+export const globalGray50 = "#f7f7f7"; // 앱 기본배경
+export const globalGray0 = "#ffffff"; // 카드/ 모달배경
+// 모달 안 텍스트필드 배경
+// 카드 안 보조 영역
+//
+// - 규칙
+// 배경이 #FFFFFF일 때만 사용
+export const globalGray100 = "#f2f3f2";
+// CTA 비활성 배경
+// 입력 불가 영역 배경
+export const globalGray150 = "#e5e5e5";
+// 카드 구분선
+// 리스트 디바이더
+// 기본 텍스트필드 테두리
+export const globalGray200 = "#e6e6e6";
+// 비활성 입력 필드
+// 비활성 카드 테두리
+export const globalGray250 = "#e3e5e3";
+export const globalGreen50 = "#ddebdd"; // 활성화된 칩 배경
+export const globalGreen200 = "#a8c1ad"; // 서브아이콘, 보조포인트
+export const globalGreen300 = "#9bbf9b"; // 메인컬러
+export const globalGreen400 = "#8e9f8e"; // 비활성(미선택)칩 텍스트
+// 활성화된 칩 텍스트
+//
+export const globalGreen700 = "#4f6f4f";
+export const globalRed50 = "#fff5f5";
+export const globalRed400 = "#e89a92";
+export const globalRed600 = "#e06a5f";
