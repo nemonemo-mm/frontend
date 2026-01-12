9f7c4288b7910453a0fca68dba57f9c47b5bf44a
kimywann <kimywan10@gmail.com>
2026-01-12 23:01:45 +0900

    chore: 리액트 쿼리 의존성 추가 및 초기 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,24 @@
 import { globalGray50 } from "@/shared/ui";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          contentStyle: { backgroundColor: globalGray50 },
-        }}
-      >
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: globalGray50 },
+          }}
+        >
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
