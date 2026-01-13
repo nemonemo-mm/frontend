1de591fd485ef9042b81f027c6066824afc6130f
SleepingOff <justjh30@gmail.com>
2026-01-13 10:46:51 +0900

    ref: 캘린더 컴포넌트 수정

--- shared/ui/organisms/Calendar.tsx
+++ shared/ui/organisms/Calendar.tsx
@@ -1,5 +1,5 @@
-import useCalendar from "@/shared/hooks/useCalendar";
-import { CalendarSchedule } from "@/shared/types/Calendar";
+import { CalendarDate, CalendarSchedule } from "@/shared/types/Calendar";
+import { GestureResponderEvent } from "react-native";
 import CalendarContainer from "../molecules/CalendarContainer";
 import CalendarDays from "../molecules/CalendarDays";
 import CalendarHeader from "../molecules/CalendarHeader";
@@ -8,26 +8,23 @@ import CalendarWeek from "../molecules/CalendarWeek";
 interface CalendarProps {
   year: number;
   month: number;
+  days: CalendarDate[][];
   schedules: CalendarSchedule[];
+  handleCalendarMonth: (dir: -1 | 1) => void;
+  onPress?: (e: GestureResponderEvent) => void;
 }
 
-const Calendar = ({ year, month, schedules }: CalendarProps) => {
-  const { goNextMonth, goPrevMonth, days, currentYearMonth } = useCalendar(
-    year,
-    month
-  );
-
-  const handleCalendarMonth = (direction: -1 | 1) => {
-    if (direction == -1) goPrevMonth();
-    else goNextMonth();
-  };
+const Calendar = ({
+  year,
+  month,
+  days,
+  schedules,
+  handleCalendarMonth,
+  onPress,
+}: CalendarProps) => {
   return (
-    <CalendarContainer>
-      <CalendarHeader
-        year={currentYearMonth.year}
-        month={currentYearMonth.month + 1}
-        goMonth={handleCalendarMonth}
-      />
+    <CalendarContainer onPress={onPress}>
+      <CalendarHeader year={year} month={month} goMonth={handleCalendarMonth} />
       <CalendarDays />
       {days.map((day, i) => (
         <CalendarWeek key={"calendar" + i} dates={day} schedules={schedules} />
