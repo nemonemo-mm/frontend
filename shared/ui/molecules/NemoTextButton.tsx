63556e0ab1ac5d01073bebc18d4bd3bbdfc15e80
SleepingOff <justjh30@gmail.com>
2026-01-15 12:52:15 +0900

    feat: 캘린더 모달에 필요한 작은 컴포넌트들 생성

---
+++ shared/ui/molecules/NemoTextButton.tsx
@@ -0,0 +1,30 @@
+import { GestureResponderEvent, StyleSheet } from "react-native";
+import { globalGray200, globalGray900, globalSpacingXs } from "..";
+import Button from "../atoms/Button";
+import NemoText from "../atoms/NemoText";
+
+interface NemoTextButtonProps {
+  onPress: (e: GestureResponderEvent) => void;
+  content: string;
+}
+
+const NemoTextButton = ({ onPress, content }: NemoTextButtonProps) => {
+  return (
+    <Button onPress={onPress} containerStyle={style.btn}>
+      <NemoText level="body3" style={{ color: globalGray900 }}>
+        {content}
+      </NemoText>
+    </Button>
+  );
+};
+const style = StyleSheet.create({
+  btn: {
+    borderRadius: globalSpacingXs,
+    backgroundColor: globalGray200 + "60",
+    paddingVertical: 7,
+    paddingHorizontal: 12,
+    marginLeft: 6,
+  },
+});
+
+export default NemoTextButton;
