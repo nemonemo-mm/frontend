269c4d056b0be49167518bc4fcbe86bffbfbdd9f
JiHyeon Lee <justjh30@gmail.com>
2026-01-22 21:15:14 +0900

    ref: 캘린더 모달 컴포넌트 상태 관리 수정 (#70)
    * ref: 캘린더 일정 추가 관련 모달 상태 정리
    
    * ref: 추가 스타일 적용 가능하도록 변경
    
    * ref: 버튼 속성 타입 확장 추가
    
    * ref: disabled 속성 추가
    
    * feat: 모달 컴포넌트 일부 책임 분리
    
    * feat: 상태 업데이트 기능 추가
    
    * fix: 컨텍스트 api로 수정
    
    * feat: 스케줄 조회 모달 컴포넌트 생성

--- shared/ui/templates/AlarmModal.tsx
+++ shared/ui/templates/AlarmModal.tsx
@@ -1,4 +1,5 @@
 import { AntDesign } from "@expo/vector-icons";
+import { useReducer } from "react";
 import { Modal, StyleSheet, View } from "react-native";
 import { globalGray700, globalGreen700 } from "..";
 import Checkbox from "../atoms/Checkbox";
@@ -6,10 +7,60 @@ import NemoText from "../atoms/NemoText";
 import BottomModal from "../organisms/BottomModal";
 
 interface AlarmModalProps {
+  initialValue: AlarmState | null;
   closeModal: () => void;
+  confirmModal: (data: typeof initialState) => void;
 }
 
-const AlarmModal = ({ closeModal }: AlarmModalProps) => {
+export type AlarmState = {
+  ten: boolean;
+  thirty: boolean;
+  sixty: boolean;
+  off: boolean;
+};
+const initialState: AlarmState = {
+  off: true,
+  ten: false,
+  thirty: false,
+  sixty: false,
+};
+
+type Action = { type: "SELECT"; key: keyof AlarmState } | { type: "RESET" };
+
+const reducer = (state: AlarmState, action: Action): AlarmState => {
+  switch (action.type) {
+    case "SELECT":
+      return {
+        ten: false,
+        thirty: false,
+        sixty: false,
+        off: false,
+        [action.key]: true,
+      };
+    case "RESET":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+const AlarmModal = ({
+  initialValue,
+  closeModal,
+  confirmModal,
+}: AlarmModalProps) => {
+  const [state, dispatch] = useReducer(reducer, initialValue ?? initialState);
+  const { off, ten, thirty, sixty } = state;
+
+  const handleAlarmState = (id: keyof typeof initialState) => {
+    if (id == "off") dispatch({ type: "RESET" });
+    dispatch({ type: "SELECT", key: id });
+  };
+
+  const handleConfirmModal = () => {
+    confirmModal(state);
+    closeModal();
+  };
   return (
     <Modal backdropColor={globalGray700 + "20"} animationType="slide">
       <BottomModal.Container>
@@ -18,26 +69,29 @@ const AlarmModal = ({ closeModal }: AlarmModalProps) => {
             <AntDesign name="close" size={20} color={globalGray700} />
           </BottomModal.LeftButton>
           <BottomModal.Title>알림</BottomModal.Title>
-          <BottomModal.RightButton>
+          <BottomModal.RightButton onPress={handleConfirmModal}>
             <AntDesign name="check" size={20} color={globalGreen700} />
           </BottomModal.RightButton>
         </BottomModal.Header>
         <View>
           <View style={style.checkboxContainer}>
             <NemoText level="body2">10분</NemoText>
-            <Checkbox value={false} handler={() => {}} />
+            <Checkbox value={ten} handler={() => handleAlarmState("ten")} />
           </View>
           <View style={style.checkboxContainer}>
             <NemoText level="body2">30분</NemoText>
-            <Checkbox value={false} handler={() => {}} />
+            <Checkbox
+              value={thirty}
+              handler={() => handleAlarmState("thirty")}
+            />
           </View>
           <View style={style.checkboxContainer}>
             <NemoText level="body2">1시간</NemoText>
-            <Checkbox value={false} handler={() => {}} />
+            <Checkbox value={sixty} handler={() => handleAlarmState("sixty")} />
           </View>
           <View style={style.checkboxContainer}>
             <NemoText level="body2">끔</NemoText>
-            <Checkbox value={false} handler={() => {}} />
+            <Checkbox value={off} handler={() => handleAlarmState("off")} />
           </View>
         </View>
       </BottomModal.Container>
--- shared/ui/templates/TimeModal.tsx
+++ shared/ui/templates/TimeModal.tsx
@@ -6,19 +6,25 @@ import BottomModal from "../organisms/BottomModal";
 import Wheel from "../organisms/Wheel";
 
 interface TimeModalProps {
+  initialValue: { hour: number; min: number };
   closeModal: () => void;
+  confirmModal: (data: { hour: number; min: number }) => void;
 }
 
-const TimeModal = ({ closeModal }: TimeModalProps) => {
-  const currentHour = new Date().getHours();
-  const currentMin = new Date().getMinutes();
-
+const TimeModal = ({
+  initialValue,
+  closeModal,
+  confirmModal,
+}: TimeModalProps) => {
   const hours = Array.from({ length: 24 }, (_, i) => i + 1);
   const mins = Array.from({ length: 60 }, (_, i) => i);
 
-  const [hour, setHour] = useState(currentHour);
-  const [min, setMin] = useState(currentMin);
-
+  const [hour, setHour] = useState(initialValue.hour);
+  const [min, setMin] = useState(initialValue.min);
+  const handleConfirmModal = () => {
+    confirmModal({ hour, min });
+    closeModal();
+  };
   return (
     <Modal backdropColor={globalGray700 + "20"} animationType="slide">
       <BottomModal.Container>
@@ -27,7 +33,7 @@ const TimeModal = ({ closeModal }: TimeModalProps) => {
             <AntDesign name="close" size={20} color={globalGray700} />
           </BottomModal.LeftButton>
           <BottomModal.Title>시간</BottomModal.Title>
-          <BottomModal.RightButton>
+          <BottomModal.RightButton onPress={handleConfirmModal}>
             <AntDesign name="check" size={20} color={globalGreen700} />
           </BottomModal.RightButton>
         </BottomModal.Header>
