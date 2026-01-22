4d79320bb72c2f8532e815f4c1d22a3991607b7f
SleepingOff <justjh30@gmail.com>
2026-01-22 18:37:33 +0900

    ref: disabled 속성 추가

--- shared/ui/organisms/TimeButton.tsx
+++ shared/ui/organisms/TimeButton.tsx
@@ -1,15 +1,25 @@
 import NemoTextButton from "../molecules/NemoTextButton";
 
 interface TimeButtonProps {
+  disabled?: boolean;
   selectedTime: { hour: number; min: number };
   handlePressTime: () => void;
 }
 
-const TimeButton = ({ selectedTime, handlePressTime }: TimeButtonProps) => {
+const TimeButton = ({
+  disabled,
+  selectedTime,
+  handlePressTime,
+}: TimeButtonProps) => {
   const hour = selectedTime.hour.toString().padStart(2, "0");
   const min = selectedTime.min.toString().padStart(2, "0");
   return (
-    <NemoTextButton content={`${hour}:${min}`} onPress={handlePressTime} />
+    <NemoTextButton
+      title=""
+      content={`${hour}:${min}`}
+      onPress={handlePressTime}
+      disabled={disabled}
+    />
   );
 };
 
