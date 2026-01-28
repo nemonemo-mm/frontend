534e42b17f3a377e1f0d86733c95249f5957c397
kimywann <kimywan10@gmail.com>
2026-01-28 14:17:00 +0900

    feat: 팀원 목록 조회 API 구현

--- features/team/api/members.ts
+++ features/team/api/members.ts
@@ -1,5 +1,9 @@
 import { apiClient } from "@/shared/utils/http";
+import { TeamMember } from "../types/team.model";
 
 export async function getTeamMembers(teamId: number) {
-  return apiClient.get(`/teams/${teamId}/members`);
+  const { data } = await apiClient.get<TeamMember[]>(
+    `/teams/${teamId}/members`
+  );
+  return data;
 }
