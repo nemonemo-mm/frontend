2a8f04ca5025bfce15d05af8a698f4e6d239b401
SleepingOff <justjh30@gmail.com>
2026-01-29 15:18:27 +0900

    ref: 날짜 비교 로직 개선

--- app/(tabs)/[teamId]/calendar/todos.tsx
+++ app/(tabs)/[teamId]/calendar/todos.tsx
@@ -8,20 +8,26 @@ interface TodosProps {}
 
 const Todos = ({}: TodosProps) => {
   const calendarContext = useContext(CalendarContext);
+  const today = new Date(Date.now());
 
   if (!calendarContext) {
     throw new Error("CalendarContext is undefined. Ensure it is provided.");
   }
 
-  const { selectedDate, days, schedules } = calendarContext;
+  const { days, schedules, todos } = calendarContext;
   const thisWeek = days
-    .filter((day) => day.some((d) => d.date == selectedDate.getDate()))
+    .filter((day) =>
+      day.some(
+        (d) =>
+          d.fullDate.getDate() == today.getDate() &&
+          d.fullDate.getMonth() == today.getMonth()
+      )
+    )
     .flat();
-
   return (
     <View>
       <CalendarDays />
-      <CalendarWeek dates={thisWeek} schedules={schedules} />
+      <CalendarWeek dates={thisWeek} schedules={[...schedules, ...todos]} />
     </View>
   );
 };
