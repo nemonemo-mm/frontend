1d1a2ca8d7374daeffbd791309741c36e8cfc24e
SleepingOff <justjh30@gmail.com>
2026-01-29 15:17:19 +0900

    feat: api 연결로 인한 추가 구현

--- app/(tabs)/[teamId]/calendar/index.tsx
+++ app/(tabs)/[teamId]/calendar/index.tsx
@@ -1,17 +1,63 @@
+import { useScheduleMutations } from "@/features/calendar/hooks/useSchedules";
+import { useTodoMutations } from "@/features/calendar/hooks/useTodos";
+import { ScheduleRequest } from "@/features/calendar/types/schedule.model";
+import { TodoRequest } from "@/features/calendar/types/todo.model";
+import {
+  PositionChip,
+  usePositions,
+} from "@/features/position/hooks/usePositions";
 import { CalendarContext } from "@/shared/hooks/useCalendarAPI";
 import Chips from "@/shared/ui/molecules/Chips";
+import { WeekDayType } from "@/shared/ui/molecules/NemoDayButton";
 import { TabsText } from "@/shared/ui/molecules/Tabs";
 import Calendar from "@/shared/ui/organisms/Calendar";
 import CalendarModal, {
+  formatAlarm,
   InitialState,
 } from "@/shared/ui/templates/CalendarModal";
-import { useContext, useState } from "react";
+import { useLocalSearchParams } from "expo-router";
+import { useContext, useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 interface CalendarScreenProps {}
 
+const convertDateAndTimeToString = (
+  dates: Date,
+  time: { hour: number; min: number }
+): string => {
+  const year = dates.getFullYear();
+  const month = dates.getMonth();
+  const date = dates.getDate();
+  const hour = time.hour;
+  const min = time.min;
+
+  return new Date(year, month, date, hour, min).toISOString();
+};
+const convertPositions = (
+  positions: PositionChip[] | undefined
+): TabsText[] => {
+  if (!positions) return [];
+  return positions.map((pos, index) => ({
+    id: pos.positionId ?? index,
+    content: pos.positionName ?? "",
+    isActive: false,
+  }));
+};
 const CalendarScreen = ({}: CalendarScreenProps) => {
-  const [currentPositions, setCurrentPositions] = useState<TabsText[]>([]);
+  const { teamId: id } = useLocalSearchParams();
+  const teamId = parseInt(id as string);
+  const { createSchedule } = useScheduleMutations();
+
+  const { createTodo } = useTodoMutations();
+
+  const positionsQuery = usePositions(teamId);
+
+  const [currentPosition, setCurrentPositions] = useState<TabsText[]>([]);
+  useEffect(() => {
+    if (positionsQuery.data) {
+      setCurrentPositions(convertPositions(positionsQuery.data));
+    }
+  }, [positionsQuery.data]);
 
   const calendarContext = useContext(CalendarContext);
 
@@ -24,11 +70,12 @@ const CalendarScreen = ({}: CalendarScreenProps) => {
     selectedDate,
     days,
     schedules,
-    setSchedules,
+    callSchedules,
+    todos,
+    callTodos,
     goPrevMonth,
     goNextMonth,
   } = calendarContext;
-
   const [isOpenAddScheduleModal, setIsOpenAddScheduleModal] = useState(false);
 
   const handleCalendarMonth = (direction: -1 | 1) => {
@@ -39,28 +86,115 @@ const CalendarScreen = ({}: CalendarScreenProps) => {
     setIsOpenAddScheduleModal(true);
   };
 
-  const handlePositionChips = (position: TabsText[]) => {
-    setCurrentPositions(position);
+  const handleConfirmModal = (data: { id: string; state: InitialState }) => {
+    const status = data.id == "schedule" ? "SCHEDULE" : "TODO";
+    const {
+      title,
+      description,
+      url,
+      startAt,
+      startAtTime,
+      endAt,
+      endAtTime,
+      person,
+      position,
+      repeat,
+      alarm,
+      isAllDay,
+    } = data.state;
+    const start = convertDateAndTimeToString(startAt, startAtTime);
+    const end = convertDateAndTimeToString(endAt, endAtTime);
+    const positionIds = position
+      .filter((pos) => pos.isActive)
+      .map((pos) => pos.id);
+    const attendeeMemberIds = person
+      .filter((per) => per.isActive)
+      .map((per) => per.id);
+    const repeatType = repeat?.period ?? "NONE";
+    let repeatEndDate = null;
+    let repeatInterval: number | null = null;
+    let repeatWeekDays: WeekDayType[] | null = null;
+    let repeatUseDate: boolean = false;
+    if (repeat) {
+      repeatEndDate = repeat.endAt.toISOString();
+      if (repeat.period == "daily") {
+        repeatInterval = repeat.interval;
+      }
+      if (repeat.period == "weekly") {
+        repeatInterval = repeat.interval;
+        repeatWeekDays = repeat.weekdays;
+      }
+      if (repeat.period == "monthly" || repeat.period == "yearly") {
+        repeatUseDate = repeat.useDate;
+      }
+    }
+
+    const alarmLabel: string = formatAlarm(alarm);
+
+    if (status == "SCHEDULE") {
+      const req = {
+        teamId,
+        title,
+        description,
+        startAt: start,
+        endAt: end,
+        isAllDay,
+        place: "",
+        url,
+        repeatType,
+        repeatInterval,
+        repeatWeekDays,
+        repeatUseDate,
+        repeatEndDate,
+        positionIds,
+        attendeeMemberIds,
+        alarm: alarmLabel,
+      } as ScheduleRequest;
+
+      createSchedule.mutate(req, {
+        onSuccess: () => {
+          callSchedules();
+        },
+      });
+    } else {
+      const req = {
+        teamId,
+        title,
+        description,
+        endAt: end,
+        place: "",
+        url,
+        assigneeMemberIds: attendeeMemberIds,
+        positionIds,
+      } as TodoRequest;
+      createTodo.mutate(req, {
+        onSuccess: () => {
+          callTodos();
+        },
+      });
+    }
+  };
+  const handlePositionChips = (next: TabsText[]) => {
+    setCurrentPositions(next);
   };
 
   return (
     <View>
       <View>
-        <Chips texts={currentPositions} handler={handlePositionChips} />
+        <Chips texts={currentPosition} handler={handlePositionChips} />
         <Calendar
           year={currentYearMonth.year}
           month={currentYearMonth.month + 1}
           days={days}
-          schedules={schedules}
+          schedules={[...schedules, ...todos]}
           onCalendarMonth={handleCalendarMonth}
           onAddSchedule={handleAddSchedule}
         />
         {isOpenAddScheduleModal && (
           <CalendarModal
             selectedDate={selectedDate}
-            confirmModal={(data: { id: string; state: InitialState }) =>
-              setSchedules((prev) => [...prev, data])
-            }
+            positions={currentPosition}
+            confirmModal={handleConfirmModal}
             closeModal={() => {
               setIsOpenAddScheduleModal(false);
             }}
