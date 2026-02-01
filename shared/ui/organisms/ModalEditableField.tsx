1b3bfcf5136fc4238d7c0023cea02418476055c2
SleepingOff <justjh30@gmail.com>
2026-02-01 12:30:20 +0900

    fix:  상태 변경 문제 해결

--- shared/ui/organisms/ModalEditableField.tsx
+++ shared/ui/organisms/ModalEditableField.tsx
@@ -43,7 +43,6 @@ const ModalEditableField = ({
   };
   const handleClickConfirm = () => {
     setIsOpenEditModal(false);
-    setCurrentInput(defaultValue ?? "");
     onConfirm(currentInput);
   };
 
