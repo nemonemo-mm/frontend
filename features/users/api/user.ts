2e54447574e380ff93c5da97e6ca9580b8af58f5
SleepingOff <justjh30@gmail.com>
2026-02-04 15:10:08 +0900

    feat: 개인 프로필 수정 api 구현

--- features/users/api/user.ts
+++ features/users/api/user.ts
@@ -1,7 +1,19 @@
 import { apiClient } from "@/shared/utils/http";
-import { UserResponse } from "../types/user.model";
+import { UserProfileResponse, UserResponse } from "../types/user.model";
 
 export async function getMe(): Promise<UserResponse> {
   const { data } = await apiClient.get<UserResponse>("/users/me");
+  return data;
+}
+
+export async function changeMyName(userName: string): Promise<UserResponse> {
+  const { data } = await apiClient.put("/users/me", { userName });
+  return data;
+}
+
+export async function changeMyProfileImage(
+  body: File
+): Promise<UserProfileResponse> {
+  const { data } = await apiClient.post("/images/users/me/profile", body);
   return data;
 }
