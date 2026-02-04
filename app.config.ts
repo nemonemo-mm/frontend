3e0ce090edf73591d45b4f3f0804cbbce431a720
SleepingOff <justjh30@gmail.com>
2026-02-04 15:41:16 +0900

    chore: 이미지 피커 패키지 추가

--- app.config.ts
+++ app.config.ts
@@ -78,6 +78,12 @@ export default {
         },
       },
     ],
+    [
+      "expo-image-picker",
+      {
+        photosPermission: "“Nemonemo” would like to access the camera.",
+      },
+    ],
     "expo-secure-store",
   ],
   experiments: {
