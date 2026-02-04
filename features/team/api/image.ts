e39ec981a8b2b02298d5b0fda0671ae0c97badad
SleepingOff <justjh30@gmail.com>
2026-02-04 16:44:28 +0900

    fix: api req 타입 수정

--- features/team/api/image.ts
+++ features/team/api/image.ts
@@ -1,6 +1,17 @@
 import { apiClient } from "@/shared/utils/http";
+import { ImagePickerAsset } from "expo-image-picker";
 
-export async function uploadTeamImage(teamId: number, image: string) {
-  const { data } = await apiClient.post(`/images/teams/${teamId}`, { image });
+export async function uploadTeamImage(teamId: number, body: ImagePickerAsset) {
+  const formData = new FormData();
+
+  formData.append("image", {
+    uri: body.uri,
+    name: body.fileName ?? "profile.jpg",
+    type: "image/jpeg", // 여기 중요
+  } as any);
+
+  const { data } = await apiClient.post(`/images/teams/${teamId}`, {
+    formData,
+  });
   return data;
 }
