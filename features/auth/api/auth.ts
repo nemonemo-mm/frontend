d5eb9022980d423b3eb0f40576994ec7cdf8ab16
kimywann <kimywan10@gmail.com>
2026-02-04 21:50:18 +0900

    feat: 회원 탈퇴 API 추가

--- features/auth/api/auth.ts
+++ features/auth/api/auth.ts
@@ -5,12 +5,16 @@ import type {
 import { apiClient } from "@/shared/utils/http";
 
 export async function socialLogin(
-  body: SocialLoginRequest
+  body: SocialLoginRequest,
 ): Promise<SocialLoginResponse> {
   const { data } = await apiClient.post<SocialLoginResponse>(
     "/auth/social/login",
-    body
+    body,
   );
 
   return data;
+}
+
+export async function deleteAccount() {
+  await apiClient.delete("/auth/user");
 }
