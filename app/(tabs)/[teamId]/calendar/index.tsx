e6856c1ddd7ec63a93b5048080e3cd800c5804be
SleepingOff <justjh30@gmail.com>
2026-02-05 16:41:06 +0900

    ref: chips 컴포넌트와 가로 스크롤용 분리

--- app/(tabs)/[teamId]/calendar/index.tsx
+++ app/(tabs)/[teamId]/calendar/index.tsx
@@ -9,7 +9,7 @@ import {
 import { CalendarContext } from "@/shared/hooks/useCalendarAPI";
 import { InitialCalendarState } from "@/shared/hooks/useCalendarForm";
 import Chip from "@/shared/ui/atoms/Chip";
-import Chips, { ChipText } from "@/shared/ui/molecules/Chips";
+import { ChipText } from "@/shared/ui/molecules/Chips";
 import { WeekDayType } from "@/shared/ui/molecules/NemoDayButton";
 import { TabsText } from "@/shared/ui/molecules/Tabs";
 import Calendar from "@/shared/ui/organisms/Calendar";
@@ -17,7 +17,7 @@ import CalendarModal from "@/shared/ui/templates/CalendarModal";
 import ScheduleListModal from "@/shared/ui/templates/ScheduleListModal";
 import { useLocalSearchParams } from "expo-router";
 import { useContext, useEffect, useState } from "react";
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 
 interface CalendarScreenProps {}
 
@@ -164,13 +164,21 @@ const CalendarScreen = ({}: CalendarScreenProps) => {
       });
     }
   };
-  const handlePositionChips = (next: ChipText[]) => {
-    setIsAll((prev) => next.every((n) => n.isActive));
-    setCurrentPositions((prev) => next);
+
+  const handlePositionChip = (id: number) => () => {
+    setIsAll(false);
+    setCurrentPositions((prev) =>
+      prev.map((chip) =>
+        chip.id === id ? { ...chip, isActive: !chip.isActive } : chip
+      )
+    );
     callSchedules();
     callTodos();
   };
 
+  useEffect(() => {
+    setIsAll(currentPosition.every((v) => v.isActive));
+  }, [currentPosition]);
   const handleConfirmListModal = (date: Date) => {
     selectDate(date);
     setIsOpenAddScheduleModal(true);
@@ -201,11 +209,30 @@ const CalendarScreen = ({}: CalendarScreenProps) => {
   return (
     <View>
       <View>
-        <View style={{ flexDirection: "row", gap: 8, marginBottom: 16 }}>
+        <View
+          style={{ flexDirection: "row", gap: 8, marginBottom: 16, width: 350 }}
+        >
           <Chip active={isAll} onPress={handlePressIsAll}>
             전체
           </Chip>
-          <Chips texts={currentPosition} handler={handlePositionChips} />
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={currentPosition}
+            keyExtractor={(text) => `chip-${text.id}`}
+            renderItem={({ item }) => (
+              <Chip
+                key={`chip-${item.id}`}
+                active={item.isActive}
+                onPress={handlePositionChip(item.id)}
+              >
+                {item.content}
+              </Chip>
+            )}
+            contentContainerStyle={{
+              gap: 8,
+            }}
+          />
         </View>
         <Calendar
           year={currentYearMonth.year}
