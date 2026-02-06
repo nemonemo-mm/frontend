de6365d57d615530dc09682832a0434a27214547
SleepingOff <justjh30@gmail.com>
2026-02-06 11:22:45 +0900

    fix: 전역 모달 삭제 및 쿼리 파람 전달

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,178 +1,16 @@
 import MainIcon from "@/assets/icons/main";
 import MyPageIcon from "@/assets/icons/mypage";
-import { useScheduleMutations } from "@/features/calendar/hooks/useSchedules";
-import { useTodoMutations } from "@/features/calendar/hooks/useTodos";
-import { ScheduleRequest } from "@/features/calendar/types/schedule.model";
-import { TodoRequest } from "@/features/calendar/types/todo.model";
-import useCalendar from "@/shared/hooks/useCalendar";
-import type { InitialCalendarState } from "@/shared/hooks/useCalendarForm";
-import { globalGray50, globalGreen300, globalGreen400 } from "@/shared/ui";
-import { WeekDayType } from "@/shared/ui/molecules/NemoDayButton";
-import CalendarModal from "@/shared/ui/templates/CalendarModal";
+import {
+  globalGray400,
+  globalGray50,
+  globalGreen300,
+  globalGreen400,
+} from "@/shared/ui";
 import { Entypo } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
-import { useCallback, useState } from "react";
-const today = new Date(Date.now());
-const year = today.getFullYear();
-const month = today.getMonth();
+import { Tabs, usePathname, useRouter } from "expo-router";
 export default function TabsLayout() {
-  const [isCalendarModalVisible, setIsCalendarModalVisible] = useState(false);
-
-  const handleOpenCalendarModal = () => {
-    setSelectedDate(new Date());
-    setIsCalendarModalVisible(true);
-  };
-
-  const handleCloseCalendarModal = () => {
-    setIsCalendarModalVisible(false);
-  };
-
-  const { createSchedule } = useScheduleMutations();
-
-  const { createTodo } = useTodoMutations();
-
-  const { goNextMonth, goPrevMonth, days, currentYearMonth } = useCalendar(
-    year,
-    month
-  );
-  // const schedulesQuery = useTeamSchedules(parseInt(teamId as string), {
-  //   start: new Date(
-  //     currentYearMonth.year,
-  //     currentYearMonth.month - 1,
-  //     1
-  //   ).toISOString(),
-  //   end: new Date(
-  //     currentYearMonth.year,
-  //     currentYearMonth.month + 2,
-  //     0
-  //   ).toISOString(),
-  // });
-
-  // const todosQuery = useTeamTodos(parseInt(teamId as string), {
-  //   start: new Date(
-  //     currentYearMonth.year,
-  //     currentYearMonth.month - 1,
-  //     1
-  //   ).toISOString(),
-  //   end: new Date(
-  //     currentYearMonth.year,
-  //     currentYearMonth.month + 2,
-  //     0
-  //   ).toISOString(),
-  // });
-
-  // const calendarSchedules = convertSchedules(schedulesQuery.data);
-  // const calendarTodos = convertTodos(todosQuery.data);
-
-  // const { data: teamDetail } = useTeamDetail(
-  //   teamId ? parseInt(teamId as string) : null
-  // );
-
-  const [selectedDate, setSelectedDate] = useState(today);
-  const selectDate = useCallback((date: Date) => {
-    setSelectedDate(date);
-  }, []);
-
-  // const callSchedules = useCallback(() => {
-  //   schedulesQuery.refetch();
-  // }, [schedulesQuery]);
-
-  // const callTodos = useCallback(() => {
-  //   todosQuery.refetch({});
-  // }, [todosQuery]);
-
-  const handleConfirmCalendarModal = (data: {
-    id: string;
-    state: InitialCalendarState;
-  }) => {
-    const {
-      teamId,
-      title,
-      description,
-      url,
-      start,
-      end,
-      person,
-      position,
-      repeat,
-      alarm,
-      isAllDay,
-    } = data.state;
-    const positionIds = position
-      .filter((pos) => pos.isActive)
-      .map((pos) => pos.positionId);
-    const attendeeMemberIds = person
-      .filter((per) => per.isActive)
-      .map((per) => per.memberId);
-    const repeatType = repeat?.period ?? "NONE";
-    let repeatEndDate = null;
-    let repeatInterval: number | null = null;
-    let repeatWeekDays: WeekDayType[] | null = null;
-    let repeatUseDate: boolean = false;
-    if (repeat) {
-      repeatEndDate = repeat.endAt.toISOString();
-      if (repeat.period == "daily") {
-        repeatInterval = repeat.interval;
-      }
-      if (repeat.period == "weekly") {
-        repeatInterval = repeat.interval;
-        repeatWeekDays = repeat.weekdays;
-      }
-      if (repeat.period == "monthly" || repeat.period == "yearly") {
-        repeatUseDate = repeat.useDate;
-      }
-    }
-    const notificationMinutes: number[] = alarm
-      ? Object.entries(alarm)
-          .map(([key, value]) => {
-            if (value) return parseInt(key);
-            else return null;
-          })
-          .filter((val): val is number => val !== null)
-      : [];
-    if (data.id == "schedule") {
-      const req = {
-        teamId,
-        title,
-        description,
-        startAt: start.toISOString(),
-        endAt: end.toISOString(),
-        isAllDay,
-        place: "",
-        url,
-        repeatType,
-        repeatInterval,
-        repeatWeekDays,
-        repeatUseDate,
-        repeatEndDate,
-        positionIds,
-        attendeeMemberIds,
-        notificationMinutes,
-      } as ScheduleRequest;
-
-      createSchedule.mutate(req, {
-        onSuccess: () => {
-          // callSchedules();
-        },
-      });
-    } else {
-      const req = {
-        teamId,
-        title,
-        description,
-        endAt: end.toISOString(),
-        place: "",
-        url,
-        assigneeMemberIds: attendeeMemberIds,
-        positionIds,
-      } as TodoRequest;
-      createTodo.mutate(req, {
-        onSuccess: () => {
-          // callTodos();
-        },
-      });
-    }
-  };
+  const pathName = usePathname();
+  const router = useRouter();
   return (
     <>
       <Tabs
@@ -194,20 +32,28 @@ export default function TabsLayout() {
           }}
         />
         <Tabs.Screen
-          name="home"
-          listeners={() => ({
-            tabPress: (event) => {
-              event.preventDefault();
-              handleOpenCalendarModal();
+          name={"home"}
+          initialParams={{ openModal: true }}
+          listeners={{
+            tabPress: (e) => {
+              e.preventDefault();
+
+              // my 탭에서는 모달 열면 안 된다면
+              if (pathName.startsWith("/my")) return;
+              if (pathName.includes("calendar"))
+                // 현재 teamId 경로에 openModal 트리거만 붙임
+                router.push(`${pathName}?openModal=true` as any);
             },
-          })}
+          }}
           options={{
             title: "",
-            tabBarIcon: ({ color }) => (
+            tabBarIcon: () => (
               <Entypo
                 name="circle-with-plus"
                 size={24}
-                color={globalGreen400}
+                color={
+                  pathName.startsWith("/my") ? globalGray400 : globalGreen400
+                }
                 style={{
                   transform: [{ translateY: -10 }, { scale: 2 }],
                   backgroundColor: globalGray50,
@@ -226,14 +72,6 @@ export default function TabsLayout() {
           }}
         />
       </Tabs>
-
-      {isCalendarModalVisible && (
-        <CalendarModal
-          selectedDate={selectedDate}
-          confirmModal={handleConfirmCalendarModal}
-          closeModal={handleCloseCalendarModal}
-        />
-      )}
     </>
   );
 }
